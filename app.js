c2744ed5efbc816e24c8b90b6cac61f21b0e3735
Annalee Switek <annaleejude@gmail.com>
2018-01-16 12:44:42 -0500

    implemented routing

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
 
-var locals = {
-  title: 'Our nunjucks example',
-  people: [
-    {name: 'Gandalf'},
-    {name: 'Frodo'},
-    {name: 'Sam'}
-  ]
-};
+app.use(volleyball); 
+app.use('/', routes);
+
 
 nunjucks.render('index.html', locals, function (err, output) {
     console.log(output);
@@ -19,13 +15,5 @@ nunjucks.render('index.html', locals, function (err, output) {
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
-
-
-app.use(volleyball);
-
-app.get('/', function (req, res){
-  res.render( 'index', locals);
-})
-
 
 app.listen(3000);
---
+++ tweetBank.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+
+var data = []; 
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  return _.cloneDeep(_.filter(ourArray, properties));
+}
+
+module.exports = { add: add, list: list, find: find };
+
+
