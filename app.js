a78b953f5043c6bacfbb4d41f9580c61df1c5485
Annalee Switek <annaleejude@gmail.com>
2018-01-16 11:15:46 -0500

    finished config excercises

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const volleyball = require('volleyball'); 
 const app = express();
+
+app.use(volleyball); 
 
 app.get('/', function (req, res){
   res.send('server listening');
