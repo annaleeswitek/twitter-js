5e87b98f73481ffae213e4658d307e049c2604d7
Kait Hoehne <kait.hoehne@gmail.com>
2018-01-16 11:46:41 -0500

    finished up with our templating engine

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const volleyball = require('volleyball'); 
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express();
 
-app.use(volleyball); 
+var locals = {
+  title: 'Our nunjucks example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Sam'}
+  ]
+};
+
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+
+app.use(volleyball);
 
 app.get('/', function (req, res){
-  res.send('server listening');
+  res.render( 'index', locals);
 })
 
 
